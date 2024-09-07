046a6b1550b68287e560ed93f66b898f1e9c4701
yardeee43 <157808346+yardeee43@users.noreply.github.com>
2024-09-07 20:32:33 +0700

    Add files via upload

---
+++ tugas3/Brio.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas3;
+
+/**
+ *
+ * @author bachtiar
+ */
+public class Brio extends Kendaraan {
+    
+    // Constructor
+    public Brio(String nama) {
+        super(nama);
+        System.out.println("nama kendaraan ini adalah "+nama);
+    }
+
+    // Meng-override method bersuara() dari kelas Hewan
+    @Override
+    public void dealer() {
+        System.out.println(nama + " dari dealer honda");
+    }
+    
+    @Override
+    public void punyaRoda(){
+        System.out.println("Roda saya 4");
+    }
+        public void gerak() {
+        System.out.println(nama + " sedang bergerak ");
+    }
+
+    // Overloading method gerak dengan parameter durasi
+    public void gerak(int kecepatan) {
+        System.out.println(nama + " bergerak dengan kecepatan "+kecepatan+" km/h");
+    }
+    
+    public void cek (){
+        System.out.println("cek override");
+        super.punyaRoda();
+        this.punyaRoda();
+    }
+}
---
+++ tugas3/Kendaraan.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas3;
+
+/**
+ *
+ * @author bachtiar
+ */
+public abstract class Kendaraan {
+      String nama;
+    
+    // Constructor
+    public Kendaraan(String nama) {
+        this.nama = nama;
+    }
+    
+    // Method abstrak (harus diimplementasikan oleh subclass)
+    public abstract void dealer();
+    
+    public void punyaRoda(){
+        System.out.println("Kendaraan ini punya roda");
+    }
+    
+    // Method non-abstrak
+    public void deskripsi() {
+        System.out.println("nama kendaraan ini adalah " + nama);
+    }
+    
+    // Method gerak yang akan di-overload
+
+
+}
